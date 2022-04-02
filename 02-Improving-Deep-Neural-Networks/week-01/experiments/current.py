Fixed some bugs in gradient checking code :-)

02-Improving-Deep-Neural-Networks/week-01/experiments/current.py
@@ -671,9 +671,8 @@ def gradient_check_n(parameters, gradients, X, Y, layer_dims, epsilon=1e-7, prin
     """
     
     # Set-up variables
-    parameters_values, _ = dictionary_to_vector(parameters)
-    
-    grad = gradients_to_vector(gradients)
+    parameters_values = parameters_to_vector(parameters, layer_dims)
+    grad = gradients_to_vector(gradients, layer_dims)
     num_parameters = parameters_values.shape[0]
     J_plus = np.zeros((num_parameters, 1))
     J_minus = np.zeros((num_parameters, 1))
@@ -687,14 +686,14 @@ def gradient_check_n(parameters, gradients, X, Y, layer_dims, epsilon=1e-7, prin
         theta_plus = np.copy(parameters_values)
         theta_plus[i] = theta_plus[i] + epsilon
         # Two steps to get cost
-        AL, _ = L_model_forward(X, vector_to_dictionary(theta_plus, layer_dims))
+        AL, _ = L_model_forward(X, vector_to_parameters(theta_plus, layer_dims))
         cost = compute_cost(AL, Y)
         J_plus[i] = cost
         
         # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
         theta_minus = np.copy(parameters_values)
         theta_minus[i] = theta_minus[i] - epsilon
-        AL, _ = L_model_forward(X, vector_to_dictionary(theta_minus, layer_dims))
+        AL, _ = L_model_forward(X, vector_to_parameters(theta_minus, layer_dims))
         cost = compute_cost(AL, Y)
         J_minus[i] = cost
         

02-Improving-Deep-Neural-Networks/week-01/experiments/dnn_utils.py
@@ -166,28 +166,25 @@ def accuracy(X, y, parameters):
 
 ################################################################################
 
-def dictionary_to_vector(parameters):
+def gradients_to_vector(gradients, layer_dims):
     """
-    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
     """
-    keys = []
+    
+    L = len(layer_dims)
+    vector = None
     count = 0
-    for key in parameters.keys():
-        
-        # flatten parameter
-        new_vector = np.reshape(parameters[key], (-1, 1))
-        keys = keys + [key] * new_vector.shape[0]
-        
+    for l in range(1, L):
         if count == 0:
-            theta = new_vector
+            vector = np.reshape(gradients['dW' + str(l)], (-1, 1))
+            count += 1
         else:
-            theta = np.concatenate((theta, new_vector), axis=0)
-        count = count + 1
+            vector = np.concatenate((vector, np.reshape(gradients['dW' + str(l)], (-1, 1))), axis=0)
+        vector = np.concatenate((vector, np.reshape(gradients['db' + str(l)], (-1, 1))), axis=0)
 
-    return theta, keys
+    return vector
 
-
-def vector_to_dictionary(theta, layer_dims):
+def vector_to_gradients(vector, layer_dims):
     """
     Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
     """
@@ -201,8 +198,50 @@ def vector_to_dictionary(theta, layer_dims):
         n_cols = layer_dims[l-1]
         W_end = cursor + (n_rows * n_cols)
         B_end = W_end + n_rows
-        parameters['W' + str(l)] = theta[cursor : W_end].reshape((n_rows, n_cols))
-        parameters['b' + str(l)] = theta[W_end: B_end].reshape((n_rows, 1))
+        parameters['dW' + str(l)] = vector[cursor : W_end].reshape((n_rows, n_cols))
+        parameters['db' + str(l)] = vector[W_end: B_end].reshape((n_rows, 1))
+        cursor = B_end
+
+        assert(parameters['dW' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['db' + str(l)].shape == (layer_dims[l], 1))
+
+
+    return parameters
+
+
+def parameters_to_vector(parameters, layer_dims):
+    """
+    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    """
+    L = len(layer_dims)
+    vector = None
+    count = 0
+    for l in range(1, L):
+        if count == 0:
+            vector = np.reshape(parameters['W' + str(l)], (-1, 1))
+            count += 1
+        else:
+            vector = np.concatenate((vector, np.reshape(parameters['W' + str(l)], (-1, 1))), axis=0)
+        vector = np.concatenate((vector, np.reshape(parameters['b' + str(l)], (-1, 1))), axis=0)
+
+    return vector
+
+def vector_to_parameters(vector, layer_dims):
+    """
+    Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+    """
+    parameters = {}
+
+    L = len(layer_dims)            # number of layers in the network
+
+    cursor = 0
+    for l in range(1, L):
+        n_rows = layer_dims[l]
+        n_cols = layer_dims[l-1]
+        W_end = cursor + (n_rows * n_cols)
+        B_end = W_end + n_rows
+        parameters['W' + str(l)] = vector[cursor : W_end].reshape((n_rows, n_cols))
+        parameters['b' + str(l)] = vector[W_end: B_end].reshape((n_rows, 1))
         cursor = B_end
 
         assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
@@ -210,23 +249,3 @@ def vector_to_dictionary(theta, layer_dims):
 
 
     return parameters
-    
-def gradients_to_vector(gradients):
-    """
-    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
-    """
-    
-    count = 0
-    for key in gradients.keys() :
-        if 'dW' in key or 'db' in key:
-            print(key)
-            # flatten parameter
-            new_vector = np.reshape(gradients[key], (-1, 1))
-            
-            if count == 0:
-                theta = new_vector
-            else:
-                theta = np.concatenate((theta, new_vector), axis=0)
-            count = count + 1
-
-    return theta